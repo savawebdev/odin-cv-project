bb7c09395a45cd7724b22b9837f3953b1aca76ef
savawedev <andrei.sava.dev@gmail.com>
2022-05-12 20:21:14 +0300

    Refactored PreviewPersonal component

--- src/components/CV/Preview/PreviewPersonal.js
+++ src/components/CV/Preview/PreviewPersonal.js
@@ -1,22 +1,17 @@
-import { Component } from "react";
 import PersonalAddress from "../../Utils/Preview/PersonalAddress/PersonalAddress";
 import PersonalInfo from "../../Utils/Preview/PersonalInfo/PersonalInfo";
 import PersonalTitle from "../../Utils/Preview/PersonalTitle/PersonalTitle";
 import PersonalWrapper from "../../Utils/Preview/PersonalWrapper/PersonalWrapper";
 
-class PreviewPersonal extends Component {
-  render() {
-    const { data } = this.props;
-
-    return (
-      <PersonalWrapper>
-        <PersonalTitle title={data.name} />
-        <PersonalAddress address={data.address} />
-        <PersonalInfo label="Email" info={data.email} />
-        <PersonalInfo label="Phone" info={data.phoneNumber} />
-      </PersonalWrapper>
-    );
-  }
-}
+const PreviewPersonal = ({ data }) => {
+  return (
+    <PersonalWrapper>
+      <PersonalTitle title={data.name} />
+      <PersonalAddress address={data.address} />
+      <PersonalInfo label="Email" info={data.email} />
+      <PersonalInfo label="Phone" info={data.phoneNumber} />
+    </PersonalWrapper>
+  );
+};
 
 export default PreviewPersonal;
