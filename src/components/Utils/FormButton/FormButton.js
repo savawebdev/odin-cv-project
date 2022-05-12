17b5ec69168d242ffe0e88ffae5cae275b33b06b
savawedev <andrei.sava.dev@gmail.com>
2022-05-12 19:39:15 +0300

    Refactored FormButton component

--- src/components/Utils/FormButton/FormButton.js
+++ src/components/Utils/FormButton/FormButton.js
@@ -1,14 +1,11 @@
-import { Component } from "react";
 import styles from "./FormButton.module.css";
 
-class FormButton extends Component {
-  render() {
-    return (
-      <button {...this.props.args} className={styles.btn}>
-        {this.props.label}
-      </button>
-    );
-  }
-}
+const FormButton = (props) => {
+  return (
+    <button {...props.args} className={styles.btn}>
+      / {props.label}
+    </button>
+  );
+};
 
 export default FormButton;
