0b31ffd9e186ee742307892cee8472254858dbf3
savawedev <andrei.sava.dev@gmail.com>
2022-05-12 20:18:43 +0300

    Refactored Preview component

--- src/components/CV/Preview/Preview.js
+++ src/components/CV/Preview/Preview.js
@@ -1,19 +1,16 @@
-import { Component } from "react";
 import PreviewEducation from "./PreviewEducation";
 import PreviewPersonal from "./PreviewPersonal";
 import Section from "../../Utils/Section/Section";
 import PreviewExperience from "./PreviewExperience";
 
-class Preview extends Component {
-  render() {
-    return (
-      <Section className="preview">
-        <PreviewPersonal data={this.props.personal} />
-        <PreviewEducation data={this.props.education} />
-        <PreviewExperience data={this.props.experience} />
-      </Section>
-    );
-  }
-}
+const Preview = (props) => {
+  return (
+    <Section className="preview">
+      <PreviewPersonal data={props.personal} />
+      <PreviewEducation data={props.education} />
+      <PreviewExperience data={props.experience} />
+    </Section>
+  );
+};
 
 export default Preview;
