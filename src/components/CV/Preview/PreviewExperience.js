95cf0ff55ff34404dccde885f4e58a695fb3a610
savawedev <andrei.sava.dev@gmail.com>
2022-05-12 20:20:21 +0300

    Refactored PreviewExperience component

--- src/components/CV/Preview/PreviewExperience.js
+++ src/components/CV/Preview/PreviewExperience.js
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import InfoWrapper from "../../Utils/Preview/InfoWrapper/InfoWrapper";
 import PreviewDate from "../../Utils/Preview/PreviewDate/PreviewDate";
 import PreviewInfo from "../../Utils/Preview/PreviewInfo/PreviewInfo";
@@ -6,24 +5,20 @@ import PreviewSection from "../../Utils/Preview/PreviewSection/PreviewSection";
 import PreviewSubTitle from "../../Utils/Preview/PreviewSubTitle/PreviewSubTitle";
 import CVSectionTitle from "../../Utils/CVSectionTitle/CVSectionTitle";
 
-class PreviewExperience extends Component {
-  render() {
-    const { data } = this.props;
+const PreviewExperience = ({ data }) => {
+  return (
+    <PreviewSection>
+      <CVSectionTitle title="Work Experience" />
 
-    return (
-      <PreviewSection>
-        <CVSectionTitle title="Work Experience" />
-
-        {data.map((work) => (
-          <InfoWrapper args={{ key: work.id }}>
-            <PreviewSubTitle name={work.company} city={work.workCity} />
-            <PreviewDate startDate={work.workStart} endDate={work.workEnd} />
-            <PreviewInfo info={`Position: ${work.position}`} />
-          </InfoWrapper>
-        ))}
-      </PreviewSection>
-    );
-  }
-}
+      {data.map((work) => (
+        <InfoWrapper args={{ key: work.id }}>
+          <PreviewSubTitle name={work.company} city={work.workCity} />
+          <PreviewDate startDate={work.workStart} endDate={work.workEnd} />
+          <PreviewInfo info={`Position: ${work.position}`} />
+        </InfoWrapper>
+      ))}
+    </PreviewSection>
+  );
+};
 
 export default PreviewExperience;
