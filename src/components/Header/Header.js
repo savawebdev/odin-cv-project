bcf682443f0a3704e9b66c6e65b524a727b8915e
savawedev <andrei.sava.dev@gmail.com>
2022-05-12 19:26:30 +0300

    Refactored Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,12 @@
-import { Component } from "react";
 import Title from "./Title/Title";
 import styles from "./Header.module.css";
 
-class Header extends Component {
-  render() {
-    return (
-      <header className={styles.header}>
-        <Title />
-      </header>
-    );
-  }
-}
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Title />
+    </header>
+  );
+};
 
 export default Header;
--- src/components/Header/Title/Title.js
+++ src/components/Header/Title/Title.js
@@ -1,10 +1,5 @@
-import { Component } from "react";
 import styles from "./Title.module.css";
 
-class Title extends Component {
-  render() {
-    return <h1 className={styles.title}>Create Your CV</h1>;
-  }
-}
+const Title = () => <h1 className={styles.title}>Create Your CV</h1>;
 
 export default Title;
