957ff597b41eae0a226164b6f86ec3364ca376a2
savawedev <andrei.sava.dev@gmail.com>
2022-05-12 20:05:27 +0300

    Refactored PreviewSubtitle component

--- src/components/Utils/Preview/PreviewSubTitle/PreviewSubTitle.js
+++ src/components/Utils/Preview/PreviewSubTitle/PreviewSubTitle.js
@@ -1,14 +1,9 @@
-import { Component } from "react";
 import styles from "./PreviewSubTitle.module.css";
 
-class PreviewSubTitle extends Component {
-  render() {
-    return (
-      <h3 className={styles["preview-subtitle"]}>
-        <span>{this.props.name}</span> - {this.props.city}
-      </h3>
-    );
-  }
-}
+const PreviewSubTitle = (props) => {
+  <h3 className={styles["preview-subtitle"]}>
+    <span>{props.name}</span> - {props.city}
+  </h3>;
+};
 
 export default PreviewSubTitle;
