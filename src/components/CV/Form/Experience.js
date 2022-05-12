7c6091efcede8a2ad8befce67bfc00db874d0f4e
savawedev <andrei.sava.dev@gmail.com>
2022-05-12 20:15:31 +0300

    Refactored Form/Experience component

--- src/components/CV/Form/Experience.js
+++ src/components/CV/Form/Experience.js
@@ -1,4 +1,4 @@
-import { Component, createRef } from "react";
+import { useRef } from "react";
 import FormButton from "../../Utils/FormButton/FormButton";
 import FormControl from "../../Utils/FormControl/FormControl";
 import FormInput from "../../Utils/FormInput/FormInput";
@@ -7,80 +7,68 @@ import CVSectionTitle from "../../Utils/CVSectionTitle/CVSectionTitle";
 import FormWrapper from "../../Utils/FormWrapper/FormWrapper";
 import uniqid from "uniqid";
 
-class Experience extends Component {
-  constructor(props) {
-    super(props);
+const Experience = (props) => {
+  const positionRef = useRef();
+  const companyRef = useRef();
+  const workCityRef = useRef();
+  const workStartRef = useRef();
+  const workEndRef = useRef();
 
-    this.positionRef = createRef();
-    this.companyRef = createRef();
-    this.workCityRef = createRef();
-    this.workStartRef = createRef();
-    this.workEndRef = createRef();
-
-    this.onSubmitHandler = this.onSubmitHandler.bind(this);
-  }
-
-  onSubmitHandler(e) {
+  const onSubmitHandler = (e) => {
     e.preventDefault();
 
     const newWorkExperience = {
       id: uniqid(),
-      position: this.positionRef.current.value,
-      company: this.companyRef.current.value,
-      workCity: this.workCityRef.current.value,
-      workStart: this.workStartRef.current.value,
-      workEnd: this.workEndRef.current.value,
+      position: positionRef.current.value,
+      company: companyRef.current.value,
+      workCity: workCityRef.current.value,
+      workStart: workStartRef.current.value,
+      workEnd: workEndRef.current.value,
     };
 
-    this.props.addWorkExperience(newWorkExperience);
-  }
+    props.addWorkExperience(newWorkExperience);
+  };
 
-  render() {
-    return (
-      <FormWrapper args={{ onSubmit: this.onSubmitHandler }}>
-        <CVSectionTitle title="Work Experience" />
+  return (
+    <FormWrapper args={{ onSubmit: onSubmitHandler }}>
+      <CVSectionTitle title="Work Experience" />
 
-        <FormControl>
-          <FormLabel htmlFor="position" labelName="Position" />
-          <FormInput
-            args={{ type: "text", name: "position", ref: this.positionRef }}
-          />
-        </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="position" labelName="Position" />
+        <FormInput
+          args={{ type: "text", name: "position", ref: positionRef }}
+        />
+      </FormControl>
 
-        <FormControl>
-          <FormLabel htmlFor="company" labelName="Company" />
-          <FormInput
-            args={{ type: "text", name: "company", ref: this.companyRef }}
-          />
-        </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="company" labelName="Company" />
+        <FormInput args={{ type: "text", name: "company", ref: companyRef }} />
+      </FormControl>
 
-        <FormControl>
-          <FormLabel htmlFor="workCity" labelName="City" />
-          <FormInput
-            args={{ type: "text", name: "workCity", ref: this.workCityRef }}
-          />
-        </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="workCity" labelName="City" />
+        <FormInput
+          args={{ type: "text", name: "workCity", ref: workCityRef }}
+        />
+      </FormControl>
 
-        <FormControl>
-          <FormLabel htmlFor="workStart" labelName="Start Date" />
-          <FormInput
-            args={{ type: "month", name: "workStart", ref: this.workStartRef }}
-          />
-        </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="workStart" labelName="Start Date" />
+        <FormInput
+          args={{ type: "month", name: "workStart", ref: workStartRef }}
+        />
+      </FormControl>
 
-        <FormControl>
-          <FormLabel htmlFor="workEnd" labelName="End Date" />
-          <FormInput
-            args={{ type: "month", name: "workEnd", ref: this.workEndRef }}
-          />
-        </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="workEnd" labelName="End Date" />
+        <FormInput args={{ type: "month", name: "workEnd", ref: workEndRef }} />
+      </FormControl>
 
-        <FormControl>
-          <FormButton args={{ type: "submit" }} label="Add" />
-        </FormControl>
-      </FormWrapper>
-    );
-  }
-}
+      <FormControl>
+        <FormButton args={{ type: "submit" }} label="Add" />
+      </FormControl>
+    </FormWrapper>
+  );
+};
 
 export default Experience;
