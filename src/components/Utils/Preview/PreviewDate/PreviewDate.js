caa89e8c8b1b54a67758264a03759370cb97926c
savawedev <andrei.sava.dev@gmail.com>
2022-05-12 20:02:52 +0300

    Refactored PreviewDate component

--- src/components/Utils/Preview/PreviewDate/PreviewDate.js
+++ src/components/Utils/Preview/PreviewDate/PreviewDate.js
@@ -1,14 +1,11 @@
-import { Component } from "react";
 import styles from "./PreviewDate.module.css";
 
-class PreviewDate extends Component {
-  render() {
-    return (
-      <small className={styles["preview-date"]}>
-        {this.props.startDate} - {this.props.endDate}
-      </small>
-    );
-  }
-}
+const PreviewDate = (props) => {
+  return (
+    <small className={styles["preview-date"]}>
+      {props.startDate} - {props.endDate}
+    </small>
+  );
+};
 
 export default PreviewDate;
