0a9e22a7b135e111f1572cce41431a4b7fd2ffaa
savawedev <andrei.sava.dev@gmail.com>
2022-05-12 19:57:31 +0300

    Refactored InfoWrapper component

--- src/components/Utils/FormButton/FormButton.js
+++ src/components/Utils/FormButton/FormButton.js
@@ -3,7 +3,7 @@ import styles from "./FormButton.module.css";
 const FormButton = (props) => {
   return (
     <button {...props.args} className={styles.btn}>
-      / {props.label}
+      {props.label}
     </button>
   );
 };
--- src/components/Utils/Preview/InfoWrapper/InfoWrapper.js
+++ src/components/Utils/Preview/InfoWrapper/InfoWrapper.js
@@ -1,14 +1,11 @@
-import { Component } from "react";
 import styles from "./InfoWrapper.module.css";
 
-class InfoWrapper extends Component {
-  render() {
-    return (
-      <div {...this.props.args} className={styles["info-wrapper"]}>
-        {this.props.children}
-      </div>
-    );
-  }
-}
+const InfoWrapper = (props) => {
+  return (
+    <div {...props.args} className={styles["info-wrapper"]}>
+      {props.children}
+    </div>
+  );
+};
 
 export default InfoWrapper;
