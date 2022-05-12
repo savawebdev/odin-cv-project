f2041727d098f12f887ffec97d6ab3f163b33eb4
savawedev <andrei.sava.dev@gmail.com>
2022-05-12 20:19:40 +0300

    Refactored PreviewEducation component

--- src/components/CV/Preview/PreviewEducation.js
+++ src/components/CV/Preview/PreviewEducation.js
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import InfoWrapper from "../../Utils/Preview/InfoWrapper/InfoWrapper";
 import PreviewDate from "../../Utils/Preview/PreviewDate/PreviewDate";
 import PreviewInfo from "../../Utils/Preview/PreviewInfo/PreviewInfo";
@@ -6,31 +5,24 @@ import PreviewSection from "../../Utils/Preview/PreviewSection/PreviewSection";
 import PreviewSubTitle from "../../Utils/Preview/PreviewSubTitle/PreviewSubTitle";
 import CVSectionTitle from "../../Utils/CVSectionTitle/CVSectionTitle";
 
-class PreviewEducation extends Component {
-  render() {
-    const { data } = this.props;
+const PreviewEducation = ({ data }) => {
+  return (
+    <PreviewSection>
+      <CVSectionTitle title="Education" />
 
-    return (
-      <PreviewSection>
-        <CVSectionTitle title="Education" />
-
-        {data.map((school) => (
-          <InfoWrapper args={{ key: school.id }}>
-            <PreviewSubTitle
-              name={school.schoolName}
-              city={school.schoolCity}
-            />
-            <PreviewDate
-              startDate={school.schoolStart}
-              endDate={school.schoolEnd}
-            />
-            <PreviewInfo info={`Degree: ${school.degree}`} />
-            <PreviewInfo info={`Subject: ${school.subject}`} />
-          </InfoWrapper>
-        ))}
-      </PreviewSection>
-    );
-  }
-}
+      {data.map((school) => (
+        <InfoWrapper args={{ key: school.id }}>
+          <PreviewSubTitle name={school.schoolName} city={school.schoolCity} />
+          <PreviewDate
+            startDate={school.schoolStart}
+            endDate={school.schoolEnd}
+          />
+          <PreviewInfo info={`Degree: ${school.degree}`} />
+          <PreviewInfo info={`Subject: ${school.subject}`} />
+        </InfoWrapper>
+      ))}
+    </PreviewSection>
+  );
+};
 
 export default PreviewEducation;
