a4609ecf960f327fd1890b3858f163c7285cefaf
savawedev <andrei.sava.dev@gmail.com>
2022-05-12 19:44:57 +0300

    Refactored SectionTitle component

--- src/components/Utils/SectionTitle/SectionTitle.js
+++ src/components/Utils/SectionTitle/SectionTitle.js
@@ -1,10 +1,5 @@
-import { Component } from "react";
 import styles from "./SectionTitle.module.css";
 
-class SectionTitle extends Component {
-  render() {
-    return <h2 className={styles.title}>{this.props.title}</h2>;
-  }
-}
+const SectionTitle = (props) => <h2 className={styles.title}>{props.title}</h2>;
 
 export default SectionTitle;
