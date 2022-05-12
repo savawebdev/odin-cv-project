7bf9bdacfd97d3baf8e7790fdc1384c952eb983f
savawedev <andrei.sava.dev@gmail.com>
2022-05-12 20:17:56 +0300

    Refactored Form/Personal component

--- src/components/CV/Form/Personal.js
+++ src/components/CV/Form/Personal.js
@@ -1,62 +1,59 @@
-import { Component } from "react";
 import FormControl from "../../Utils/FormControl/FormControl";
 import FormInput from "../../Utils/FormInput/FormInput";
 import FormLabel from "../../Utils/FormLabel/FormLabel";
 import CVSectionTitle from "../../Utils/CVSectionTitle/CVSectionTitle";
 import FormWrapper from "../../Utils/FormWrapper/FormWrapper";
 
-class Personal extends Component {
-  render() {
-    return (
-      <FormWrapper>
-        <CVSectionTitle title="Personal Information" />
+const Personal = (props) => {
+  return (
+    <FormWrapper>
+      <CVSectionTitle title="Personal Information" />
 
-        <FormControl>
-          <FormLabel htmlFor="name" labelName="Name" />
-          <FormInput
-            args={{
-              type: "text",
-              name: "name",
-              onChange: this.props.onChange,
-            }}
-          />
-        </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="name" labelName="Name" />
+        <FormInput
+          args={{
+            type: "text",
+            name: "name",
+            onChange: props.onChange,
+          }}
+        />
+      </FormControl>
 
-        <FormControl>
-          <FormLabel htmlFor="address" labelName="Address" />
-          <FormInput
-            args={{
-              type: "text",
-              name: "address",
-              onChange: this.props.onChange,
-            }}
-          />
-        </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="address" labelName="Address" />
+        <FormInput
+          args={{
+            type: "text",
+            name: "address",
+            onChange: props.onChange,
+          }}
+        />
+      </FormControl>
 
-        <FormControl>
-          <FormLabel htmlFor="email" labelName="Email" />
-          <FormInput
-            args={{
-              type: "email",
-              name: "email",
-              onChange: this.props.onChange,
-            }}
-          />
-        </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="email" labelName="Email" />
+        <FormInput
+          args={{
+            type: "email",
+            name: "email",
+            onChange: props.onChange,
+          }}
+        />
+      </FormControl>
 
-        <FormControl>
-          <FormLabel htmlFor="phoneNumber" labelName="Phone Number" />
-          <FormInput
-            args={{
-              type: "number",
-              name: "phoneNumber",
-              onChange: this.props.onChange,
-            }}
-          />
-        </FormControl>
-      </FormWrapper>
-    );
-  }
-}
+      <FormControl>
+        <FormLabel htmlFor="phoneNumber" labelName="Phone Number" />
+        <FormInput
+          args={{
+            type: "number",
+            name: "phoneNumber",
+            onChange: props.onChange,
+          }}
+        />
+      </FormControl>
+    </FormWrapper>
+  );
+};
 
 export default Personal;
