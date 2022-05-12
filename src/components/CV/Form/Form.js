1fe5c5036f175e57952bcddbf532f33302c856c1
savawedev <andrei.sava.dev@gmail.com>
2022-05-12 20:16:12 +0300

    Refactored Form component

--- src/components/CV/Form/Form.js
+++ src/components/CV/Form/Form.js
@@ -1,21 +1,18 @@
-import { Component } from "react";
 import Education from "./Education";
 import Experience from "./Experience";
 import Personal from "./Personal";
 import Section from "../../Utils/Section/Section";
 import SectionTitle from "../../Utils/SectionTitle/SectionTitle";
 
-class Form extends Component {
-  render() {
-    return (
-      <Section className="form">
-        <SectionTitle title="Enter Your Information" />
-        <Personal onChange={this.props.onPersonalChange} />
-        <Education addSchool={this.props.addSchool} />
-        <Experience addWorkExperience={this.props.addWorkExperience} />
-      </Section>
-    );
-  }
-}
+const Form = (props) => {
+  return (
+    <Section className="form">
+      <SectionTitle title="Enter Your Information" />
+      <Personal onChange={props.onPersonalChange} />
+      <Education addSchool={props.addSchool} />
+      <Experience addWorkExperience={props.addWorkExperience} />
+    </Section>
+  );
+};
 
 export default Form;
